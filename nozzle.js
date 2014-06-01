8a6bdd055e84c131dabb5c90d030a996be2ce063
David/Zitao Zhang <gwwfps@gmail.com>
2014-06-01 03:50:13 -0400

    circular dependency checking

--- nozzle.js
+++ nozzle.js
@@ -17,27 +17,14 @@
   };
 
   var Injector = function() {
-    this.factories = {};
-    this.constants = {};
+    this.values = {};
   };
 
-  Injector.prototype.factory = function(name, factory) {
-    this.factories[name] = factory;
-  };
+  var TYPE_FACTORY = 'factory';
+  var TYPE_SINGLETON = 'singleton';
+  var TYPE_CONSTANT = 'constant';
 
-  Injector.prototype.singleton = function(name, factory) {
-    this.constants[name] = this.resolve(factory)();
-  };
-
-  Injector.prototype.constant = function(name, value) {
-    this.constants[name] = value;
-  };
-
-  var _circularCheck = function(names) {
-    
-  };
-
-  Injector.prototype.resolve = function(func) {
+  var _parseDependencies = function(func, type) {    
     var args;
     if (_isArray(func)) {
       args = func.slice(1, func.length);
@@ -47,6 +34,53 @@
       args = func.toString().match(FN_ARGS)[1].split(',');
     }
 
+    return {
+      dependencies: args,
+      func: func,
+      type: type
+    };
+  };
+
+  Injector.prototype.factory = function(name, factory) {
+    this.values[name] = _parseDependencies(factory, TYPE_FACTORY);
+  };
+
+  Injector.prototype.singleton = function(name, factory) {
+    this.values[name] = _parseDependencies(factory, TYPE_SINGLETON);
+  };
+
+  Injector.prototype.constant = function(name, value) {
+    this.values[name] = {
+      value: value,
+      type: TYPE_SINGLETON
+    };
+  };
+
+  var _checkDependencies = function(name, visited) {
+    var value = this.values[name];
+    if (value === void 0) {
+      throw new Error("Undefined dependency: " + name);
+    }
+
+    if (value.type === TYPE_CONSTANT) {
+      return;
+    }
+
+    var chain = visited.concat([name]);
+    if (_arrayContains(visited, name)) {
+      throw new Error("Cycle found in dependency chain: " + chain);
+    }
+    var dependencies  = value.dependencies;
+    for (var i = 0; i < dependencies.length; i++) {
+      _checkDependencies(dependencies[i], chain);
+    }
+  };
+
+  Injector.prototype.resolve = function(func) {
+    var parsed = _parseDependencies(func);
+    for (var i = 0; i < parsed.dependencies.length; i++) {
+      
+    }
   };
   
   Injector.prototype.extend = function(anotherInjector) {
@@ -65,6 +99,19 @@
   // Utility function for determining if an object is an `Array`.
   var _isArray = Array.isArray || function(obj) {
     return Object.prototype.toString.call(obj) === '[object Array]';
+  };
+
+  var _arrayContains = function (array, element) {
+    if (Array.prototype.indexOf) {
+      return array.indexOf(element) !== -1;
+    } else {
+      for (var i = 0; i < array.length, i++) {
+        if (array[i] === element) {
+          return true;
+        }
+      }
+      return false;
+    }
   };
 
 }).call(this);
