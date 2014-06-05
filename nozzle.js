a4bee190beeb7827bb5dc15eeaeb53944fb131e2
David/Zitao Zhang <gwwfps@gmail.com>
2014-06-05 01:40:45 -0400

    basic logic

--- nozzle.js
+++ nozzle.js
@@ -2,29 +2,23 @@
   var global = this;
 
   var Nozzle = function() {
-
-  };
-
-  var DEFAULT_INJECTOR_NAME = 'default';
+    this.values = {};
+  };  
 
   Nozzle.injectors = {};
 
-  Nozzle.injector = function(name) {
+  Nozzle.get = function(name) {
     if (this.injectors[name] === void 0) {
-      this.injectors[name] = new Injector();
+      this.injectors[name] = new Nozzle();
     }
     return this.injectors[name];
-  };
-
-  var Injector = function() {
-    this.values = {};
   };
 
   var TYPE_FACTORY = 'factory';
   var TYPE_SINGLETON = 'singleton';
   var TYPE_CONSTANT = 'constant';
 
-  var _parseDependencies = function(func, type) {    
+  Nozzle.prototype._parseDependencies = function(func, type) {    
     var args;
     if (_isArray(func)) {
       args = func.slice(1, func.length);
@@ -41,22 +35,22 @@
     };
   };
 
-  Injector.prototype.factory = function(name, factory) {
-    this.values[name] = _parseDependencies(factory, TYPE_FACTORY);
+  Nozzle.prototype.factory = function(name, factory) {
+    this.values[name] = this._parseDependencies(factory, TYPE_FACTORY);
   };
 
-  Injector.prototype.singleton = function(name, factory) {
-    this.values[name] = _parseDependencies(factory, TYPE_SINGLETON);
+  Nozzle.prototype.singleton = function(name, factory) {
+    this.values[name] = this._parseDependencies(factory, TYPE_SINGLETON);
   };
 
-  Injector.prototype.constant = function(name, value) {
+  Nozzle.prototype.constant = function(name, value) {
     this.values[name] = {
       value: value,
       type: TYPE_SINGLETON
     };
   };
 
-  var _checkDependencies = function(name, visited) {
+  Nozzle.prototype._checkDependencies = function(name, visited) {
     var value = this.values[name];
     if (value === void 0) {
       throw new Error("Undefined dependency: " + name);
@@ -72,36 +66,62 @@
     }
     var dependencies  = value.dependencies;
     for (var i = 0; i < dependencies.length; i++) {
-      _checkDependencies(dependencies[i], chain);
+      this._checkDependencies(dependencies[i], chain);
     }
   };
 
-  Injector.prototype.resolve = function(func) {
-    var parsed = _parseDependencies(func);
-    for (var i = 0; i < parsed.dependencies.length; i++) {
-      
+  Nozzle.prototype._getValue = function(name) {
+    _checkDependencies(name, []);
+
+    var parsedValue = this.values[name];
+
+    if (parsedValue.type === TYPE_CONSTANT) {
+      return parsedValue.value;
     }
+
+    var initialized = this._injectParsed(parsedValue)();
+
+    if (parsedValue.type === TYPE_SINGLETON) {
+      this.values[name] = {
+        value: initialized,
+        type: TYPE_CONSTANT
+      };
+    }
+
+    return initialized;
   };
-  
-  Injector.prototype.extend = function(anotherInjector) {
 
-  };
-
-  Injector.prototype.inject = function(func) {
-
-    var services =  [];
-
+  Nozzle.prototype._injectParsed = function(parsedValue) {
     return function() {
-      return func.apply(this, services);
+      var depNames = parsedValue.dependencies;
+      var depName;
+      var dependencies = [];
+      for (var i = 0; i < depNames.length; i++) {
+        depName = depNames[i];
+        dependencies.push(this._getValue(depName));
+      }
+      return parsedValue.func.apply(this, dependencies);
     };
   };
 
-  // Utility function for determining if an object is an `Array`.
-  var _isArray = Array.isArray || function(obj) {
-    return Object.prototype.toString.call(obj) === '[object Array]';
+  Nozzle.prototype.inject = function(func) {
+    var parsed = this._parseDependencies(func);
+    return this._injectParsed(parsed);
   };
 
-  var _arrayContains = function (array, element) {
+  // Utility functions for determining object types
+
+  var _toStringTypeComp = function(typeName) {
+    return function(obj) {
+      return Object.prototype.toString.call(obj) === '[object ' + typeName + ']';
+    };    
+  };
+
+  var _isArray = Array.isArray || _toStringTypeComp('Array');
+  var _isString = _toStringTypeComp('String');
+  var _isFunction = _toStringTypeComp('Function');
+
+  var _arrayContains = function(array, element) {
     if (Array.prototype.indexOf) {
       return array.indexOf(element) !== -1;
     } else {
@@ -111,6 +131,32 @@
         }
       }
       return false;
+    }
+  };
+
+  var _isInjectable = function(obj) {
+    if (_isFunction(obj)) {
+      return true;
+    }
+
+    if (_isArray(obj)) {
+      return _isFunction(obj[obj.length-1]);
+    }
+
+    return false;
+  };
+
+  var defaultNozzle = new Nozzle();  
+
+  global.zz = function(arg1, arg2) {    
+    if (_isString(arg1)) {
+      if (_isInjectable(arg2)) {
+        defaultNozzle.factory(arg1, arg2);
+      } else {
+        defaultNozzle.constant(arg1, arg2);
+      }
+    } else {
+      defaultNozzle.inject(arg1);
     }
   };
 
