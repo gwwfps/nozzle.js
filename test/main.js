e5abaf8c28add87fda1911bbdfb60232986f2f2e
David/Zitao Zhang <gwwfps@gmail.com>
2014-07-10 00:05:50 -0400

    constant and singleton test

--- test/main.js
+++ test/main.js
@@ -8,10 +8,40 @@ suite('Main', function() {
         return {value: 1};
     });
 
-    var injected = nozzle.inject(function (f1) {
+    var injected = nozzle.inject(function(f1) {
         return f1.value;
     });
 
     assert.equal(1, injected());
+  });
+
+  test('#constant', function() {
+    var nozzle = new Nozzle();
+    nozzle.constant('c1', {value: 1});
+
+    var injected = nozzle.inject(function(c1) {
+        return c1.value;
+    });
+
+    assert.equal(1, injected());
+  });
+
+  test('#singleton', function() {
+    var nozzle = new Nozzle();
+    nozzle.singleton('s1', function() {
+        return {value: 1};
+    });
+
+    var injected = nozzle.inject(function(s1) {
+        return s1.value;
+    });
+
+    assert.equal(1, injected());
+
+    nozzle.inject(function(s1) {
+        s1.value = 2;
+    })();
+
+    assert.equal(2, injected());
   });
 });
